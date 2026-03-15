d1e9b34255b8666cca2a65206370fc69abd91c0a
Lucia <lhmr11@hotmail.com>
2026-03-15 11:10:05 -0300

    Adicionei testes para GET transferências total e individual

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -5,12 +5,14 @@ const postTransferencias = require('../fixtures/postTransferencias.json')
 require('dotenv').config()
 
 describe('Transferencias', () => {
+
+    let token
+    beforeEach(async () => {
+        //capturar o token
+        token = await obterToken('julio.lima', 123456)
+    })
+
     describe('POST /transferencias', () => {
-        let token
-        beforeEach(async () => {
-            //capturar o token
-            token = await obterToken('julio.lima', 123456)
-        })
 
         it('Deve retornar sucesso com 201, quando o valor da transferencias for igual ou acima de R$10,00', async () => {
 
@@ -36,5 +38,32 @@ describe('Transferencias', () => {
         })
 
     })
+    describe('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso com 200 e dados iguais ao registro de transferencias contido no banco de dados, quando o ID for valido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/4')
+                .set('Authorization', `Bearer ${token}`)
 
+            expect(resposta.status).to.equal(200);
+            expect(resposta.body.id).to.be.equal(4);
+            expect(resposta.body.id).to.be.a('number');
+            expect(resposta.body.conta_origem_id).to.equal(1);
+            expect(resposta.body.conta_destino_id).to.equal(2);
+            expect(resposta.body.valor).to.equal(11.00);
+            expect(resposta.body.conta_origem_id).to.be.a('number');
+
+
+        })
+    })
+    describe('GET /transferencias', () => {
+        it('Deve retornar 4 elementos na paginação quando informar limite de 4 registros', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=4') // isso pegamos do postman porem la tava 10, mas ja sei que meu bd tem só 4
+                .set('Authorization', `Bearer ${token}`)
+
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(4)
+            expect(resposta.body.transferencias).to.have.length(4)
+        })
+    })
 })
